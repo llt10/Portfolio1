a9df35f59b3b2a20ab2e15824630723ac1d22da7
Leanne  Thoppil <leannelthoppil@gmail.com>
2025-09-17 10:28:01 -0500

    saving work 9/17

---
+++ tictactoe.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <vector>
+
+//welcome message
+int main () {
+    std::cout << "Welcome to the Tic Tac Toe Game!!" << std::endl;
+    return 0;
+}
+
+//display an array and assign each square with numbers --3x3-- (1-9)
+void displayTable (vector<string> a, vector vector<string> b){
+    
+    for (auto firstRow = 0; firstRow !=b.size(); firstRow++) 
+    { 
+        for (auto col =0; col != b[firstRow].size(); col++)
+    }
+    if (col==0)
+    {
+        std::cout << " " << std::endl;
+    }
+        std::cout << 
+} 
+//research how to display grid because idk how 
+
+//cin be the numbers 1-9 then you replace that with 
+
+//replace number with X (Player 1) or O (Player 2) 
+
+//Which player won the game display this message
+//ending message
+int main () {
+    std::cout << "Thank you for playing Tic Tac Toe!!" << std::endl;
+    return 0; 
+}
+
+
+
+
