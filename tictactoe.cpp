470a477006c59540873d9ae45123b45c229c48cd
Leanne  Thoppil <leannelthoppil@gmail.com>
2025-09-21 21:47:53 -0500

    .....

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -19,11 +19,11 @@ char showCell(const std::vector<char>& board, int i) {
 
 void displayTable(const std::vector<char>& board) {
 
-    std::cout << " " << showCell(board, 0) << " | " << showCell(board, 1) << " | " << showCell(board, 2) << "\n"<< std::endl;
+    std::cout << " " << showCell(board, 0) << " | " << showCell(board, 1) << " | " << showCell(board, 2) << std::endl;
     std::cout << "---+---+---\n";
-    std::cout << " " << showCell(board, 3) << " | " << showCell(board, 4) << " | " << showCell(board, 5) << "\n"<< std::endl;
+    std::cout << " " << showCell(board, 3) << " | " << showCell(board, 4) << " | " << showCell(board, 5) << std::endl;
     std::cout << "---+---+---\n";
-    std::cout << " " << showCell(board, 6) << " | " << showCell(board, 7) << " | " << showCell(board, 8) << "\n" << std::endl;
+    std::cout << " " << showCell(board, 6) << " | " << showCell(board, 7) << " | " << showCell(board, 8) 1<< std::endl;
     std::cout << "\n" << std::endl;
 }
 
