2254b82f56baa436b27670a85d5e1c46f173c5b9
Leanne  Thoppil <leannelthoppil@gmail.com>
2025-10-21 10:45:17 -0500

    adding second portfolio battlefield

---
+++ battle.hpp
@@ -0,0 +1,17 @@
+#ifndef BATTLE_HPP
+#define BATTLE_HPP
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+
+bool apprMoveChar(char c);
+
+char promptForMove(int playerNumber, char otherPlayerMove);
+
+std::string promptArchetype(int playerNumber);
+
+int countMoves(const std::vector<char>& board);
+
+bool alchemSwap (std::vector<char>& board);
